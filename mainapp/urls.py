017d8f57c4c0b98216208ffdc22fc530a4bc7353
Yosuke Suzuki <yosuke.suzuki@gmail.com>
2014-01-09 06:31:28 +0900

    add Rest API for jplayerdata

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,21 +2,41 @@
 # mainapp.urls
 # 
 
-import urllib
+#import urllib
 
-from google.appengine.api import memcache
+#from google.appengine.api import memcache
 
 from kay.routing import (
   ViewGroup, Rule
 )
-from kay.generics import crud
-from kay.generics import admin_required
+#from kay.generics import crud
+#from kay.generics import admin_required
 
-from adminapp.forms import AdminPageForm
-from mainapp.models import AdminPage
+from kay.exceptions import NotAuthorized
+from kay.generics import (
+  OP_LIST, OP_SHOW, OP_CREATE, OP_UPDATE, OP_DELETE
+)
+
+from kay.generics.rest import RESTViewGroup
+
+#from adminapp.forms import AdminPageForm
+from mainapp.models import JPlayerData
 from mainapp.views import CACHE_NAME_FOR_TOP_PAGE_RESULTS
 
+def read_only(self, request, operation, obj=None, model_name=None,
+        prop_name=None):
+    if operation == OP_CREATE or operation == OP_UPDATE or \
+            operation == OP_DELETE:
+        raise NotAuthorized()
+
+
+class JDataRESTViewGroup(RESTViewGroup):
+    models = ['mainapp.models.JPlayerData']
+    fetch_page_size = 1000
+    authorize = read_only 
+
 view_groups = [
+  JDataRESTViewGroup(),
   ViewGroup(
     Rule('/', endpoint='index', view='mainapp.views.index'),
     Rule('/bad/request/', endpoint='bad_request', view='mainapp.views.bad_request'),
